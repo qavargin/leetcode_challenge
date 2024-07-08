3fedf06cd66d5b1a2dac923f23188798c7d16a6d
German Vargin <germanvargin@mbp-german.speedport.ip>
2024-07-08 09:47:20 +0200

    Completed "remove element" task

--- arrays/27_remove_element.py
+++ arrays/27_remove_element.py
@@ -1,4 +1,17 @@
 from typing import List
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are 
+not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted,
+you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are 
+not equal to val. The remaining elements of nums are not important as well as the size of nums.
+
+Return k.
+"""
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         k = 0  # Pointer to track the position for elements not equal to val
@@ -11,7 +24,7 @@ class Solution:
         return k
 
 # Test this solution:
-nums = [3, 2, 2, 3]
+nums = [3, 2, 2, 3, 3]
 val = 3
 solution = Solution()
 k = solution.removeElement(nums, val)
